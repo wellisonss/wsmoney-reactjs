f3d8336dd5e267646097cf32cdcbadb61b5ff629
wellison santos <silva.s.wellison@gmail.com>
2022-05-12 20:37:12 -0300

    adicionando estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlocalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
      <h1>Hello Word</h1>
+     <GlocalStyle />
     </div>
   );
 }
