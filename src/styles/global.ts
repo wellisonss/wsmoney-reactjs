5f2ea080ea50f5ff2958d43257422a507a478399
wellison santos <silva.s.wellison@gmail.com>
2022-05-12 21:58:18 -0300

    modificação do global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,12 @@ import { createGlobalStyle } from 'styled-components';
 export const GlocalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #FFFFFF;
   }
 
   * {
@@ -24,5 +30,14 @@ export const GlocalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
   }
 `
