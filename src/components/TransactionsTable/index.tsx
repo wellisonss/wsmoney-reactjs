ae5af33f0b496a9299c99b8015788c277fc910b0
wellison santos <silva.s.wellison@gmail.com>
2022-05-14 20:01:39 -0300

    adicionando tabela

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from "./style"
+
+export function TransactionTable(){
+  return (
+    <Container>
+      <table>
+          <thead>
+            <tr>
+              <th>Título</th>
+              <th>Preço</th>
+              <th>Categoria</th>
+              <th>Data</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Camisa</td>
+              <td className="withdraw">-10.00</td>
+              <td>Compra</td>
+              <td>10/10/2022</td>
+            </tr>
+            <tr>
+              <td>Camisa</td>
+              <td className="deposit">10.00</td>
+              <td>Venda</td>
+              <td>10/10/2022</td>
+            </tr>
+          </tbody>
+      </table>
+    </Container>
+  )
+}
